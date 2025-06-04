656dcdcb750514b9b97d6b47ae1f0dc296c11600
Dev1-web <nervesparksdev@gmail.com>
2025-06-04 15:51:13 +0530

    changed port

--- app.py
+++ app.py
@@ -1481,5 +1481,5 @@ if __name__ == "__main__":
     app.logger.info("=" * 80 + "\n")
 
     # Run Flask app
-    port = int(os.environ.get("PORT", 3000))
+    port = int(os.environ.get("PORT", 4000))
     app.run(host="0.0.0.0", port=port, debug=True)  # debug=True is fine for development
